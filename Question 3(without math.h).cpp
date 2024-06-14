b6ea3ab241320c9fe3c26c66d3618d4601e5630b
dibikapandey00 <dibikapandey25@gmail.com>
2024-06-15 00:18:10 +0545

    Add files via upload

---
+++ Question 3(without math.h).cpp
@@ -0,0 +1,35 @@
+// Q.No.3  Write a program to find prime factors of a number.
+
+
+
+#include <stdio.h>
+
+void primeFactors(int n) {
+    
+    while (n % 2 == 0) {
+        printf("%d ", 2);
+        n = n / 2;
+    }
+
+    
+    for (int i = 3; i * i <= n; i = i + 2) {
+       
+        while (n % i == 0) {
+            printf("%d ", i);
+            n = n / i;
+        }
+    }
+
+    
+    if (n > 2)
+        printf("%d ", n);
+}
+
+int main() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    printf("Prime factors of the number are: ");
+    primeFactors(n);
+    return 0;
+}
